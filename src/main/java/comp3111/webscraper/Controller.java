9b17cb98ba6151522e2f11a15b59009dee41cf9e
bryanchun <chun.hiu.sang@gmail.com>
2018-11-05 00:24:50 +0800

    Split initTable and related methods to TableTab class

--- src/main/java/comp3111/webscraper/Controller.java
+++ src/main/java/comp3111/webscraper/Controller.java
@@ -12,16 +12,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import javafx.collections.ListChangeListener;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.util.Callback;
-import javafx.application.HostServices;
 
 import java.util.Date;
 import java.util.List;
@@ -66,20 +57,21 @@ public class Controller {
     @Deprecated
     private List<Item> refineResult;
 
+    // TableTab nodes
     @FXML
-    private VBox root;
+    public VBox root;
     @FXML
-    private TableView<Item> table;
+    public TableView<Item> table;
     @FXML
-    private TableColumn<Item, String> titleColumn;
+    public TableColumn<Item, String> titleColumn;
     @FXML
-    private TableColumn<Item, Double> priceColumn;
+    public TableColumn<Item, Double> priceColumn;
     @FXML
-    private TableColumn<Item, String> urlColumn;
+    public TableColumn<Item, String> urlColumn;
     @FXML
-    private TableColumn<Item, Date> postedDateColumn;
+    public TableColumn<Item, Date> postedDateColumn;
     @FXML
-    private TableColumn<Item, String> portalColumn;
+    public TableColumn<Item, String> portalColumn;
 
     /**
      * StringProperty storing text that is shown in the console TextArea
@@ -116,8 +108,9 @@ public class Controller {
             // Updates isRefineDisabled
             isRefineDisabled.setValue(newValue.getHasSearchRefined() || currentProducts.size() == 0);
         });
+
         // Initialize Table factories and listeners
-        initTable();
+        new TableTab(this).initTable(currentProducts);
     }
     
     /**
@@ -147,61 +140,6 @@ public class Controller {
     @FXML
     private void actionNew() {
     	System.out.println("actionNew");
-    }
-
-    private <S,T> Callback<TableColumn<S,T>, TableCell<S,T>> newURLCellFactory() {
-        return param -> new TableCell<S,T>(){
-            {
-                this.setOnMouseClicked(event -> openURL(this.getText()));
-            }
-
-            @Override
-            protected void updateItem(T t, boolean bln) {
-                super.updateItem(t, bln);
-                setText(t!= null ? t.toString() : "");
-            }
-        };
-    };
-
-    /**
-     * Called when Controller initialize, instead of when Table Tab is clicked.
-     */
-    private void initTable() {
-        // Initialize mapping between table column text and Item attributes for setting list of Items to table
-        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
-        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-        urlColumn.setCellValueFactory(new PropertyValueFactory<>("url"));
-        postedDateColumn.setCellValueFactory(new PropertyValueFactory<>("createdAt"));
-        portalColumn.setCellValueFactory(new PropertyValueFactory<>("portal"));
-
-        // Pop up a new windows/browser showing the item when the URL is clicked.
-        urlColumn.setCellFactory(newURLCellFactory());
-
-        // Sort the result in ascending order on user clicking each column, and sort in descending order when user click again.
-        // Note that after pressing twice, the third click does nothing and resets so that the next click gives ascending sort.
-        this.table.getColumns().forEach(
-            column -> column.setSortable(true)
-        );
-
-        // Update table items by and on the change of data in observed list
-        currentProducts.addListener((ListChangeListener<Item>) change -> {
-            System.out.println("currentProducts updated");
-            //System.out.println("Changed on " + change);
-            this.table.setItems(currentProducts);
-        });
-
-        // Make cells not editable
-        this.table.setEditable(false);
-    }
-
-    /**
-     * Helper function for opening a URL from a browser
-     * @param url
-     */
-    private void openURL(String url) {
-        Stage stage = (Stage) root.getScene().getWindow();
-        HostServices hostServices = (HostServices) stage.getProperties().get("hostServices");
-        hostServices.showDocument(url);
     }
 }
 
